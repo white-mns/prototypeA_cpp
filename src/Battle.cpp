9da06ce77036dac855e05fea0dbbc38b5938af2c
shiro <shiro.mns@gmail.com>
2019-02-06 13:26:53 +0900

    [追加]キャラクタークラスを結果出力に使用

--- src/Battle.cpp
+++ src/Battle.cpp
@@ -1,4 +1,5 @@
 #include "Battle.h"
+#include "Character.h"
 #include <iostream>
 
 namespace BattleSystem{
@@ -9,10 +10,15 @@ namespace BattleSystem{
     }
     
     void Battle::Execute() {
+        Character* friend_0 = new Character("味方A");
+        Character* enemy_0  = new Character("敵A");
+
         std::string test = "";
 
-        test += "戦闘開始！\n";
+        test += "味方PT | " + friend_0->GetName() + "\n";
+        test += "敵PT | " + enemy_0->GetName()  + "\n";
 
+        test += "戦闘開始！\n";
         test += "戦闘終了！\n";
 
         std::cout << test << std::endl;
