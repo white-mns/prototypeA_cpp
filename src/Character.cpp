23fdaad934fb3a0e80bb5570a544e5a82f79a976
shiro <shiro.mns@gmail.com>
2019-02-06 13:23:31 +0900

    [追加]キャラクタークラス

---
+++ src/Character.cpp
@@ -0,0 +1,34 @@
+#include "Character.h"
+#include <iostream>
+#include <string>
+
+namespace BattleSystem{
+
+    Character::Character(void) {
+        Initialize(0, "---", 1500, 50);
+    }
+
+    Character::Character(std::string name) {
+        Initialize(0, name, 1500, 50);
+    }
+
+    Character::~Character(void) {
+    }
+
+    
+    /***************************************************
+    *      キャラクターデータの初期化
+    *****************************************************/
+    void Character::Initialize(int characterId, std::string name, int mhp, int attack) {
+        this->characterId = characterId;
+        this->name        = name;
+        this->mhp         = mhp;
+        this->hp          = mhp;
+        this->attack      = attack;
+    }
+
+    std::string Character::GetName() {
+        return this->name;
+    }
+}
+
---
+++ src/Character.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <string>
+
+namespace BattleSystem{
+
+    class Character{
+        public:
+            Character(void);
+            Character(std::string name);
+            ~Character(void);
+
+            bool CheckDefeated();
+
+            void Initialize(int characterId, std::string name, int mhp, int attack);
+            std::string GetName();
+
+        protected:
+            int characterId;
+            std::string name;
+            int mhp;
+            int hp;
+            int attack;
+    };
+}
+
