f7d518678969cdac02a62f242a646893be53146e
shiro <shiro.mns@gmail.com>
2019-02-09 11:19:20 +0900

    [変更]戦闘不能判定を行動直前に移行

--- src/Turn.cpp
+++ src/Turn.cpp
@@ -25,7 +25,7 @@ namespace BattleSystem{
         result += "　味方PT | " + character_0->GetName() + "　" + std::to_string(character_0->GetHp()) + "/" + std::to_string(character_0->GetMhp()) + "\n";
         result += "　敵PT | "   + character_1->GetName() + "　" + std::to_string(character_1->GetHp()) + "/" + std::to_string(character_1->GetMhp())  + "\n";
 
-        { // 味方の行動
+        if (!character_0->CheckDefeated()){ // 味方の行動
             Action* act = new Action(this->character_0, this->character_1);
 
             result += act->Execute();
@@ -33,11 +33,7 @@ namespace BattleSystem{
             delete act;
         }
 
-        if (character_1->CheckDefeated()) {
-            return result;
-        }
-
-        { // 敵の行動
+        if (!character_1->CheckDefeated()){ // 敵の行動
             Action* act = new Action(this->character_1, this->character_0);
 
             result += act->Execute();
